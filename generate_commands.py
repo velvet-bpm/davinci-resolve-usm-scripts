e00c9735367d4d5929ed07daa85ddc419284923a
amxn <187657976+velvet-bpm@users.noreply.github.com>
2026-09-16 05:33:27 +0200

    Refactored the code
    Refactored the code to allow for multiple of the same effect to be recognized, as before only the las entry was accepted do to how the code was structured

--- generate_commands.py
+++ generate_commands.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from effect_relations import id_and_name
+from collections import defaultdict
 import os
 folder_path = os.path.join(os.environ["USERPROFILE"],"Documents")
 new_file_path = os.path.join(folder_path, "SeCommands.lua")
@@ -13,22 +14,23 @@ timeline_frame_rate = current_project.GetSetting("timelineFrameRate")
 
 audio_track_list_items = current_timeline.GetItemListInTrack('audio',2)
 
-effect_id_and_start_frames = {}
+effect_id_and_start_frames = defaultdict(list)
+frames = []
 
 if audio_track_list_items:
     for item in audio_track_list_items:
         for key in id_and_name:
             if id_and_name.get(key) == item.GetName().split('.')[0]:
-                effect_id_and_start_frames[key] = round((item.GetStart() - timeline_start_frame) * 60 / timeline_frame_rate)
-                
-                #print(f"{key}: {(effect_id_and_start_frames.get(key))}")
+                effect_id_and_start_frames[key].append(f"{round((item.GetStart() - timeline_start_frame) * 60 / timeline_frame_rate)}")
 else:
     print("No item in track list")
+
 
 if effect_id_and_start_frames:
     if new_file_path:
                 os.remove(new_file_path)
     for effect in effect_id_and_start_frames:
-        with open(new_file_path, "a+") as file:
-            file.write(f'playSeVer2(spep_0 + {effect_id_and_start_frames.get(effect)}, {effect}, {""}, 0, 0, 0, -1 ) --{id_and_name.get(effect)}\n')
+        for frame in effect_id_and_start_frames[effect]:
+            with open(new_file_path, "a+") as file:
+                file.write(f'playSeVer2(spep_0 + {frame}, {effect}, "", 0, 0, 0, -1 ) --{id_and_name.get(effect)}\n')
 
